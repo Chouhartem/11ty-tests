82a7488b6b2dca546161f6cf600c1878c4fd1258
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2023-10-27 16:32:13 +0200

    Client-side generation

--- content/index.md
+++ content/index.md
@@ -1,8 +1,17 @@
+---
+title: main page
+layout: layouts/base.njk
+---
+
 # Home
 
-```mermaid
+This is a test page for mermaid.
+
+Here follows a mermaid diagram:
+
+{% mermaid %}
 graph LR
 Document --> A[K<sub>A</sub> ] & B[K<sub>B</sub> ]
 A --> K_C1[K<sub>C1</sub>]
 B --> K_C2[K<sub>C2</sub>]
-```
+{% endmermaid %}
--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,12 @@
 const markdownIt = require("markdown-it");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 module.exports = function(eleventyConfig) {
+  eleventyConfig.addWatchTarget("_styles");
+  eleventyConfig.addPassthroughCopy("_styles");
+  eleventyConfig.addPassthroughCopy('node_modules/mermaid/');
+
   eleventyConfig.addGlobalData("site", {
     lang: "en",
     title: "Test eleventy",
@@ -14,9 +20,16 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.setLibrary("md", markdownIt(options));
 
+  eleventyConfig.addPairedShortcode("mermaid", function(data) {
+    var container = new JSDOM('<pre class="mermaid"></pre>');
+    var pre = container.window.document.querySelector('.mermaid');
+    pre.innerHTML = data;
+    return container.window.document.body.innerHTML;
+  });
+
   return {
     dir: {
-      includes: "_includes",
+      includes: "../_includes",
       input: "content",
       output: "output"
     }
