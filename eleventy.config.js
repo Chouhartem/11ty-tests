cb51779d9bf8ead1b67f33d660fc63ef75d6b5bc
Fabrice Mouhartem <fabrice.mouhartem@xwiki.com>
2023-10-27 14:34:31 +0200

    Templates?

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,24 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function(eleventyConfig) {
-	return {
-		dir: {
-			input: "content",
-			output: "output"
-		}
-	};
+  eleventyConfig.addGlobalData("site", {
+    lang: "en",
+    title: "Test eleventy",
+  });
+
+  let options = {
+    html: true,
+    breaks: false,
+    linkify: true
+  };
+
+  eleventyConfig.setLibrary("md", markdownIt(options));
+
+  return {
+    dir: {
+      includes: "_includes",
+      input: "content",
+      output: "output"
+    }
+  };
 };
